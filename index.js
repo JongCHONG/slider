cfde01254c3e13f24a689eb7f899a641b5364816
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-20 16:50:36 +0200

    Randompics

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 $(function() {
     var count = 0
+    for (var i = 1; i <= 5; i++) {
+        $(`.slide:nth-child(${i})`).css("background-image", `url("https://picsum.photos/1600/900?t=${Math.random()}")`)
+    }
+
     $('.point').click(function() {
+        count = $(this).index() * -1
         $(".slider").animate({
             left: `-${$(this).index()*100}%`
         })
@@ -23,17 +28,19 @@ $(function() {
             left: `${count*100}%`
         })
     })
+
     setInterval(sliding, 5000)
-// setTimeout(function() {
-    
-// }, 2000);
-function sliding() {
-    count = count - 1
-    if (count === -5) {
-        count = 0
+    function sliding() {
+        count = count - 1
+        if (count === -5) {
+            count = 0
+        }
+        if (count === 1) {
+            count = -4
+        }
+        $(".slider").animate({
+            left: `${count*100}%`
+        })
     }
-    $(".slider").animate({
-        left: `${count*100}%`
-    })
-}
+
 })
